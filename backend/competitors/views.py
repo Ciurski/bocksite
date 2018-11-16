8b8c10761277406582454c2385442cc25d5a5894
Ciurski <s.bidowaniec@gmail.com>
2018-11-16 22:21:51 +0100

    custom user create method, next matching with competitor

--- backend/competitors/views.py
+++ backend/competitors/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 # Create your views here.
-from rest_framework import generics, viewsets
-
+from rest_framework import generics, viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+#from rest_framework.request import Request
 from .models import Competitor
 from .serializers import CompetitorSerializer, UserSerializer, GroupSerializer
 
@@ -21,6 +23,19 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
+    def create(self, request, format=None):
+        """
+        If possible match existing competitor and group to user.
+        Else create new competitor and match to new user.
+        """
+        competitors = Competitor.objects.all()
+        data = request.data
+        serializer_context = {'request': request}
+        serializer = UserSerializer(data=data, context=serializer_context)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class GroupViewSet(viewsets.ModelViewSet):
     """
